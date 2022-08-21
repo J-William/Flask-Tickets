c2595fde27de8e0adbb55f09c1c32045b45e9729
Will Seymour <willseymour804@gmail.com>
2022-08-21 17:02:02 -0500

    updated .gitignore

---
+++ application/__init__.py
@@ -0,0 +1,40 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    """ App factory."""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    if test_config is None:
+        # Load the instance config if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if one is passed
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Initialize the db
+    from . import db
+    db.init_app(app)
+    
+    # Authorization blueprint
+    from . import auth
+    app.register_blueprint(auth.bp)
+    from . import index
+    app.register_blueprint(index.bp)
+    from . import admin
+    app.register_blueprint(admin.bp)
+    
+    
+    
+    return app
+        
